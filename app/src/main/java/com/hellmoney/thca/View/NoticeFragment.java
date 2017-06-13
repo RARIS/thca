2b276046112fb88f4237fa15f2f7499bacaa1805
Len <joenggyu0@gmail.com>
2017-06-13 17:12:13 +0900

    # UI 아이템 완성 그리고 메인 프래그먼트 빼고 완 (#1)

---
+++ app/src/main/java/com/hellmoney/thca/View/NoticeFragment.java
@@ -0,0 +1,67 @@
+package com.hellmoney.thca.View;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.hellmoney.thca.R;
+
+public class NoticeFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+
+    public NoticeFragment() {
+        // Required empty public constructor
+    }
+
+    public static NoticeFragment newInstance(String param1, String param2) {
+        NoticeFragment fragment = new NoticeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+    public void onButtonPressed(Uri uri) {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public interface OnFragmentInteractionListener {
+    }
+}
